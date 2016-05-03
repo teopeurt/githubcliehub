8720af0e653ebdb03adeba13d4708e66089f0aba
don Onwunum <me@teopeurt.com>
2016-05-03 15:47:10 +0100

    Initial Commit

---
+++ GitClieHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitClieHub
+//
+//  Created by don Onwunum on 03/05/2016.
+//  Copyright © 2016 pigstycoders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
